00665a729d92b784d368d575d581122d8162f912
OriShmueli <43569659+OriShmueli@users.noreply.github.com>
2024-02-07 20:03:25 +0200

    more ui

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import serial
 
 window = tk.Tk()
 #window.attributes('-fullscreen', True)
-window.state('zoomed')
+#window.state('zoomed')
+window.geometry("1024x600")
+
+isConnected = False
+
+def ConnectToArduino():
+    connectButton["state"] = "disabled"
+    startButton.config(bg='gray')
+ 
+connectButton = tk.Button(window, text='Connect', command=ConnectToArduino, bg='green')
+connectButton.place(x=80, y=110)
+
 
 operation = True
 
@@ -27,7 +38,46 @@ startButton = tk.Button(window, text='Start operation', command=Operation, bg='g
 startButton.place(x=50, y=50)
 
 #list box
-serialPortListBox = tk.Listbox(window)
+#serialPortListBox = tk.Listbox(window)
 
 
+#Connecteion status Labels
+connectionStatusLabelTitle = tk.Label(window, text = "Connection status: ")
+connectionStatusLabelTitle.pack()
+
+arduinoMotorConnectionStatusLabel = tk.Label(window, text = "arduino motor: ")
+arduinoMotorConnectionStatusLabel.pack()
+
+arduinoSensorsConnectionStatusLabel = tk.Label(window, text = "arduino sensors: ")
+arduinoSensorsConnectionStatusLabel.pack()
+
+#Connection status text
+arduinoMotorConnectionStatusText = tk.Text(window)
+arduinoSensorsConnectionStatusText = tk.Text(window)
+    #TODO: place them together 
+
+#distance sensor
+distanceSensorLabel = tk.Label(window, text = "Distance: ")
+distanceSensorLabel.pack()
+
+distanceSensorText = tk.Text(window)
+
+#color sensor
+colorSensorLabel = tk.Label(window, text = "Color: ")
+colorSensorLabel.pack()
+
+colorSensorText = tk.Text(window)
+
+#conveyor belt status
+conveyorBeltStatusLabel = tk.Label(window, text = "Conveyor Belt Status: ")
+conveyorBeltStatusLabel.pack()
+
+conveyorBeltStatusText = tk.Text()
+
+#error timer
+errorTimerArduinoSensorsLabel = tk.Label(window, text = "Error Timer: ")
+errorTimerArduinoMotorLabel = tk.Label(window, text = "Error Timer: ")
+
+errorTimerArduinoSensorsText = tk.Label(window)
+errorTimerArduinoMotorText = tk.Label(window)
 window.mainloop()
